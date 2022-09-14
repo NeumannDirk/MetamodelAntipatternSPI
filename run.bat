a3e54e3a164a52d22c16338847c2fa3b7bff0ce5
DirkNeumann <dirkneumann98@gmail.com>
2022-09-14 23:22:22 +0200

    Remove .bat files from repository and add them to .gitignore

--- run.bat
+++
@@ -1,3 +0,0 @@
-java -Djava.ext.dir=C:/ProgramFiles/Java/jre1.8.0_271/lib/ext;./analyzerApi/target;./analyzerImpl/target;./analyzerImplAdvanced/target;./analyzerImpl/target/depends -cp ./analyzerApi/target/analyzerApi-0.0.1-SNAPSHOT.jar;./analyzerImpl/target/analyzerImpl-0.0.1-SNAPSHOT.jar;./analyzerImplAdvanced/target/analyzerImplAdvanced-0.0.1-SNAPSHOT.jar;./mainanalyzer/target/mainanalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar mainanalyzer.MainAnalyzer -header -inputDirectory "D:/data/ap_mm" -outputDirectory "D:/" -log 0
-
-pause
